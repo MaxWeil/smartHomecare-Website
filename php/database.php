846f5bbef28ebe48f1aa7c624368c120f618f613
MaxWeil <zwecki99@gmail.com>
2017-12-22 22:10:04 +0100

    Mobile First - Login Page
    - start of redesigning/rewriting the whole project
    - using "mobile first" tactic for responsive web design
    - login page for smallest device finished

---
+++ php/database.php
@@ -0,0 +1,7 @@
+<?php
+  $dbServer = "localhost";
+  $dbUsername = "root";
+  $dbPassword = "";
+  $dbName = "smarthomecare";
+  $connection = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
+ ?>
