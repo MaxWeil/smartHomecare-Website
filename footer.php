bafd099737cff71a97b601d6d1465f92169a0e01
MaxWeil <zwecki99@gmail.com>
2017-12-22 15:14:44 +0100

    v1.1
    new Master Branch Version 1.1 started

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-  <span>&copy; smartHomecare - Dimploma Project: Maximilian Kapper | Noah Zuchna | Maximilian Weilbuchner</span>
-</footer>
--- php/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $dbServer = "localhost";
-  $dbUsername = "root";
-  $dbPassword = "";
-  $dbName = "smarthomecare";
-
-  $connection = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
- ?>
